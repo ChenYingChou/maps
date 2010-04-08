c456f32fbf3fe1a76714bbc1a765e5ddb779d7a3
Tony Chen <tonychen@finenet.com.tw>
2010-04-08 20:05:52 +0800

    Fix comment of AttrTypeMap.php, add notes

--- AttrTypeMap.php
+++ AttrTypeMap.php
@@ -15,7 +15,7 @@ _TEXT_;
 <button id="mapTrigger_{$oAttribute->name}">Map</button><div></div>
 <input type="hidden" name="content[attributes][attr_id][$key]" value="{$oAttribute->id}" />
 <script type="text/javascript">
-$('#mapTrigger_{$oAttribute->name}').click(AttrTypeMapFunc);
+$('#mapTrigger_{$oAttribute->name}').click(function(){AttrTypeMapFunc(this)});
 </script>
  */
 ?>
